18369de5a11e594c59053febb57d13a5d0168b45
Aidan Collins <AidanCollins55@gmail.com>
2025-04-18 14:15:40 -0600

    Project2_Functions assignment

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,10 @@
+function My_First_Function() {
+    var str = "This is the button text!";
+    document.getElementById("Button_Text") .innerHTML = str;
+}
+
+function My_Function() {
+    var sentence = "I am learning JavaScript";
+    sentence += "and there is a lot to learn!";
+    document.getElementById("sentence") .innerhtml = sentence;
+} //this adds a second button that when clicked shows the sentence above
