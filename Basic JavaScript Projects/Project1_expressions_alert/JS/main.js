04ce01582211217a8e96f48e27d549dac80b12fe
Aidan Collins <AidanCollins55@gmail.com>
2025-04-18 13:45:55 -0600

    Intro to JavaScript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!");
+var A = "This is a string" ;
+var Dog = "Labrador", Cat = "Simese", Squirrel = "Flying", Snake = "Anaconda"; //sets my variables to different animal types
+document.write(Dog); //writes the var Dog which is a labrador
+document.write(7 + 7); //executes addition for a sum of 14
+var Sent1 = "This is the beginning of the string";
+var Sent2 = "and this is the end string";
+var B = "This is the beginning of the string" + "and this is the end of the string";
+document.write(B);
