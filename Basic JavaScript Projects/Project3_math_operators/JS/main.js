dd2d48ecf6f260fdb6674977467ec210babb42c1
Aidan Collins <AidanCollins55@gmail.com>
2025-04-24 15:34:18 -0600

    Project3

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,14 +1,14 @@
-function power_operator() { //Does the math operation "to the power of"//
+function power_operator () { //Does the math operation "to the power of"//
     var power = 20, 2;
     document.getElementById("Power").innerHTML = Math.pow(20,2);
 }
 
-function modulus_Operator() { 
+function modulus_Operator () { 
     var simple_Math = 25 % 6;
     document.getElementById("Math").innerHTML + simple_Math;
 }
 
-function multiplication() { //A function for multiplication
+function multiplication () { //A function for multiplication
     var multiplication = 6 * 8;
     document.getElementById("Multiply").innerHTML = "6 x 8 = " + multiplication;
 }
@@ -18,9 +18,23 @@ function division () { //A function for division
     document.getElementById("Divide").innerHTML = "48 / 6 = " + division;
 }
 
-function subtraction_function() { //A function for subtraction
+function subtraction_function () { //A function for subtraction
     var subtraction = 5 - 2;
     document.getElementById("Subtract").innerHTML = "5 - 2 = " + subtraction;
 }
 
+{
+var x = 5;
+x++;
+document.write(x);
+}
+
+{
+var y = 5.55;
+x--;
+document.write(y)
+}
+{
+document.getElementById("Random").innerHTML = Math.random();
+}
 window.alert(Math.random() * 100);
